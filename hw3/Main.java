1a738787e1b7b507d5d7a4ac1a1ce55ad61d1226
NazrinGasimova <165589947+NazrinGasimova@users.noreply.github.com>
2025-02-02 01:27:51 +0400

    Add files via upload

---
+++ hw3/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Pet pet=new Pet("cat","maya",2,54, new String[]{"meow", "eat", "sleep"});
+        Human mother=new Human("Zehra","Kaya",1981);
+        Human father=new Human("Yusuf","Kaya",1981);
+        Human human=new Human("Hayat","Kaya",2005,father,mother,pet);
+
+        human.greetPet();
+        human.describePet();
+        pet.eat();
+        pet.foul();
+        pet.respond();
+
+    }
+}
