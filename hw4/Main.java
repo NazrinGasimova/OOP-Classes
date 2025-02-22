d13ecf9868a5e4a7bba82ac41a31a23784f66e05
NazrinGasimova <165589947+NazrinGasimova@users.noreply.github.com>
2025-02-22 21:53:54 +0400

    Add files via upload

--- hw4/Main.java
+++ hw4/Main.java
@@ -2,15 +2,26 @@ public class Main {
     public static void main(String[] args) {
         Pet pet = new Pet(Species.CAT, "Maya", 2, 54, new String[]{"meow", "eat", "sleep"});
 
+        String[][] schedule = {
+                {DayOfWeek.MONDAY.name(), "Go for a morning run"},
+                {DayOfWeek.TUESDAY.name(), "Practice guitar"},
+                {DayOfWeek.WEDNESDAY.name(), "Attend a coding workshop"},
+                {DayOfWeek.THURSDAY.name(), "Try a new recipe"},
+                {DayOfWeek.FRIDAY.name(), "Watch a documentary"},
+                {DayOfWeek.SATURDAY.name(), "Go hiking"},
+                {DayOfWeek.SUNDAY.name(), "Have a family game night"}
+        };
+
         Human mother = new Human("Zehra", "Kaya", 1981);
         Human father = new Human("Yusuf", "Kaya", 1981);
+
 
         Family family = new Family(mother, father);
         family.setPet(pet);
 
         Human child = new Human("Hayat", "Kaya", 2005, 90, null);
         family.addChild(child);
-
+        child.setSchedule(schedule);
         System.out.println(family);
 
         for (int i = 0; i < 100000; i++) {
