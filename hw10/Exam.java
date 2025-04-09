d8986546928b5999638e3cc02eb9b2f89857d335
NazrinGasimova <165589947+NazrinGasimova@users.noreply.github.com>
2025-04-09 22:18:19 +0400

    Add files via upload

---
+++ hw10/Exam.java
@@ -0,0 +1,23 @@
+package lesson6;
+
+public class Exam {
+    private String id;
+    private Course subject;
+    private Student examinee;
+    private int result;
+
+    public Exam(String id, Course subject, Student examinee, int result) {
+        this.id = id;
+        this.subject = subject;
+        this.examinee = examinee;
+        this.result = result;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getResult() {
+        return result;
+    }
+}
